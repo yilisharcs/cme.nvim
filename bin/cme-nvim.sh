9679004f32df9e75a8ad227732faaba60d12b0b2
yilisharcs <yilisharcs@gmail.com>
2025-11-19 22:15:01 -0300

    style: string newlines and indentation

--- bin/cme-nvim.sh
+++ bin/cme-nvim.sh
@@ -14,18 +14,16 @@ cleanup() {
         echo
 
         if [ $exit_code -eq 0 ]; then
-            echo -e "Compilation finished" \
-                    "at ${end_time},"     \
-                    "duration ${duration}"
+                echo -e "Compilation finished at ${end_time}, duration ${duration}"
         elif [ $exit_code -ge 128 ]; then
-            if [ "$exit_code" -eq 254 ]; then
-                signal_num=2
-            else
-                signal_num=$((exit_code - 128))
-            fi
-            echo -e "Compilation exited abnormally with signal ${signal_num} at ${end_time}, duration ${duration}"
+                if [ "$exit_code" -eq 254 ]; then
+                        signal_num=2
+                else
+                        signal_num=$((exit_code - 128))
+                fi
+                echo -e "Compilation exited abnormally with signal ${signal_num} at ${end_time}, duration ${duration}"
         else
-            echo -e "Compilation exited abnormally with code ${exit_code} at ${end_time}, duration ${duration}"
+                echo -e "Compilation exited abnormally with code ${exit_code} at ${end_time}, duration ${duration}"
         fi
 
         trap - EXIT
@@ -37,8 +35,8 @@ trap cleanup EXIT INT
 # Get the shell argument and remove it from the list
 CME_SHELL="$1"; shift
 
-echo -e "-*- directory: $(pwd | sed "s#^${HOME}#~#") -*-"
-echo -e "Compilation started at ${START_TIME}\n"
+echo -e "-*- directory: $(pwd | sed "s#^${HOME}#~#") -*-\n\n" \
+        "Compilation started at ${START_TIME}\n"
 
 if [ $# -eq 0 ]; then
         echo -e "Argument required."
