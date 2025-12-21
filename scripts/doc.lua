36b61669293843157cff40532a45dcd6b9547f97
yilisharcs <yilisharcs@gmail.com>
2025-12-21 19:11:35 -0300

    build: apparently I'm supposed to call mini.doc.setup? idk

--- scripts/doc.lua
+++ scripts/doc.lua
@@ -1,5 +1,9 @@
 package.path = "./vendor/mini-doc/lua/?.lua" .. package.path
-require("mini.doc").generate({
+
+local minidoc = require("mini.doc")
+minidoc.setup()
+
+minidoc.generate({
         "lua/cme/init.lua",
         "plugin/cme.lua",
 }, "doc/cme.nvim.txt", {})
